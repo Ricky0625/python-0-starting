f59729e4f76f0fc1861b7126da696b4f69e2375f
Ricky0625 <rickywong0625@gmail.com>
2023-09-19 18:12:30 +0800

    updated ex09

--- ex09/setup.py
+++ ex09/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="ft_package",
     version="0.0.1",
-    description="A Python package that exists solely for the purpose of existing",
+    description="A package that exists solely for the purpose of existing",
     url="https://github.com/ricky0625/ft_package",
     author="Ricky Wong",
-    author_email="<rickywong0625@gmail.com>",
+    author_email="rickywong0625@gmail.com",
     license="MIT",
     packages=find_packages(),
     classifiers=[],
